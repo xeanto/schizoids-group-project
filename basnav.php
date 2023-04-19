f95304eeac355372c5fd2e94a06df52262075fb7
BEIQIN CHI <beiqinchi@macbook-pro-161.wireless.rit.edu>
2023-04-19 14:53:39 -0400

    bolding issue is being fixed

--- basnav.php
+++ basnav.php
@@ -12,7 +12,7 @@
     <img id="" src="" alt="">
     <h3>Opening a file: </h3>
 	<ul>
-		<li><p>To open a file with Nano, simply type the command <b>‘nano’<b> followed by the name of the file you want to open. For example, to open a file called <b>example.txt<b>, type the command <b>‘nano example.txt’<b>.</p></li>
+		<li><p>To open a file with Nano, simply type the command <b>‘nano’</b> followed by the name of the file you want to open. For example, to open a file called <b>example.txt<b>, type the command <b>‘nano example.txt’<b>.</p></li>
 		<br>
 		<li><p>If the file does not exist, Nano will create a new file with that name.</p></li>
 	</ul>
@@ -26,18 +26,18 @@
 
 	<h3>Scrolling the screen: </h3>
 	<ul>
-		<li><p>To scroll the screen in Nano, use the <b>Ctrl<b> key in combination with the arrow keys. The <b>Ctrl<b> key and <b>Up<b> arrow key scrolls the screen up one line, the <b>Ctrl<b> key and <b>Down<b> arrow key scrolls the screen down one line, the <b>Ctrl<b> key and <b>Left<b> arrow key scrolls the screen left, and the <b>Ctrl<b> key and <b>Right<b> arrow key scrolls the screen right.</p></li>
+		<li><p>To scroll the screen in Nano, use the <b>Ctrl</b> key in combination with the arrow keys. The <b>Ctrl<b> key and <b>Up<b> arrow key scrolls the screen up one line, the <b>Ctrl<b> key and <b>Down<b> arrow key scrolls the screen down one line, the <b>Ctrl<b> key and <b>Left<b> arrow key scrolls the screen left, and the <b>Ctrl<b> key and <b>Right<b> arrow key scrolls the screen right.</p></li>
 		<br>
 	</ul>
     <h3>Searching for text: </h3>
 	<ul>
-		<li><p>To search for text in Nano, use the keyboard shortcut <b>Ctrl+W<b>. This will open the search prompt, where you can enter the text you want to search for. Press <b>Enter<b> to start the search.</p></li>
+		<li><p>To search for text in Nano, use the keyboard shortcut <b>Ctrl+W</b>. This will open the search prompt, where you can enter the text you want to search for. Press <b>Enter<b> to start the search.</p></li>
 		<br>
-        <li><p>To search for the next occurrence of the text, use the keyboard shortcut <b>Ctrl+W<b> again.</p></li>
+        <li><p>To search for the next occurrence of the text, use the keyboard shortcut <b>Ctrl+W</b> again.</p></li>
 	</ul>
 	<h3>Exiting Nano: </h3>
 	<ul>
-		<li><p>To exit Nano, use the keyboard shortcut <b>Ctrl+X<b>. If you have made changes to the file, Nano will prompt you to save the changes before exiting.</p></li>
+		<li><p>To exit Nano, use the keyboard shortcut <b>Ctrl+X</b>. If you have made changes to the file, Nano will prompt you to save the changes before exiting.</p></li>
 	</ul>
 </main>
 
