d7ac3f3fd87ee78663b834f0d3f9adcc01c6e76b
Casey <casey@xeanto.us>
2023-04-19 15:57:50 -0400

    add if state for post

--- assets/inc/comments.php
+++ assets/inc/comments.php
@@ -1,9 +1,24 @@
 <?php
 include('/home/iste240t51/Sites/pinclude/dbCon.php');
-$sql = "SELECT * FROM comments";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get the comment from the form
+    $comment = $_POST["comment"];
+    $name = $_POST["name"];
+    // Prepare the SQL statement to insert the comment into the `comments` table
+    $sql = "INSERT INTO comments(message, name) VALUES ('$comment','$name')";
+  
+    // Execute the SQL statement
+    if ($mysqli->query($sql) === TRUE) {
+      echo "<p class='green'>Comment saved successfully</p>";
+    } else {
+      echo "Error: " . $sql . "<br>" . $mysqli->error;
+    }
+  } else {
+$sql = "SELECT * FROM comments LIMIT 5";
 $results = $mysqli->query($sql)->fetch_all();
 foreach ($results as $result) {
     echo $result;
 }
+  }
 $mysqli->close();
 ?>
