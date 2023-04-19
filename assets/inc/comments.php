873e12de5e8d80e425d9f73feb892308ff33489a
Casey <casey@xeanto.us>
2023-04-19 15:54:43 -0400

    fixed include path for database connectin

--- assets/inc/comments.php
+++ assets/inc/comments.php
@@ -1,5 +1,9 @@
 <?php
-include('../../pinclude/dbCon.php');
-
+include('/home/iste240t51/Sites/pinclude/dbCon.php');
+$sql = "SELECT * FROM comments";
+$results = $mysqli->query($sql)->fetch_all();
+foreach ($results as $result) {
+    echo $result;
+}
 $mysqli->close();
 ?>
