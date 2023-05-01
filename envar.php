dab3101849c8c37856c625f941989de6fc398452
Casey Malley <casey@xeanto.us>
2023-05-01 17:52:03 -0400

    change environmental to environment

--- envar.php
+++ envar.php
@@ -9,41 +9,41 @@ include($path . "assets/inc/nav.php");
 
 
 <main>
-	<h2>Environmental variables</h2>
+	<h2>Environment variables</h2>
 	<img id="" src="" alt="">
 
-	<h3>Viewing environmental variables: </h3>
+	<h3>Viewing environment variables: </h3>
 	<ul>
-		<p>To view the list of environmental variables in your system, use the command <b>‘printenv’</b> or
-			<b>‘env’</b>. This will display a list of all the environmental variables and their values.
+		<p>To view the list of environment variables in your system, use the command <b>‘printenv’</b> or
+			<b>‘env’</b>. This will display a list of all the environment variables and their values.
 		</p>
 	</ul>
 	<br>
-	<h3>Setting environmental variables: </h3>
+	<h3>Setting environment variables: </h3>
 	<ul>
-		<p>To set an environmental variable, use the <b>‘export’</b> command followed by the variable name and value.
+		<p>To set an environment variable, use the <b>‘export’</b> command followed by the variable name and value.
 			For example, to set the <b>‘PATH’</b> variable to include a new directory, use the command <b>‘export
 				PATH=$PATH:/new/directory’</b>. This will add the new directory to the system's <b>‘PATH’</b> variable,
 			allowing you to execute commands from that directory.</p>
 	</ul>
 	<br>
-	<h3>Unsetting environmental variables: </h3>
+	<h3>Unsetting environment variables: </h3>
 	<ul>
-		<p>To unset an environmental variable, use the <b>‘unset’</b> command followed by the variable name. For
+		<p>To unset an environment variable, use the <b>‘unset’</b> command followed by the variable name. For
 			example, to unset the <b>‘PATH’</b> variable, use the command <b>‘unset PATH’</b>.</p>
 	</ul>
 	<br>
-	<h3>Using environmental variables: </h3>
+	<h3>Using environment variables: </h3>
 	<ul>
-		<p>Environmental variables can be used in many ways in Unix. For example, you can use the <b>‘HOME’</b> variable
+		<p>Environment variables can be used in many ways in Unix. For example, you can use the <b>‘HOME’</b> variable
 			to navigate to your home directory, the <b>‘USER’</b> variable to display your username, and the
 			<b>‘PS1’</b> variable to customize your shell prompt.
 		</p>
 	</ul>
 	<br>
-	<h3>Persistent environmental variables: </h3>
+	<h3>Persistent environment variables: </h3>
 	<ul>
-		<p>To make environmental variables persistent across sessions, you can add them to the appropriate startup file
+		<p>To make environment variables persistent across sessions, you can add them to the appropriate startup file
 			for your shell. For example, for the Bash shell, you can add variables to the <b>‘.bashrc’</b> file. For the
 			Zsh shell, you can add variables to the <b>‘.zshrc’</b> file.</p>
 	</ul>
@@ -54,10 +54,10 @@ include($path . "assets/inc/nav.php");
 
 <?php
 // quiz variables
-$title = "Environmental variables Quiz";
+$title = "Environment variables Quiz";
 $quizQuestions = [
-	0 => "What command can you use to set the value of an environmental variable?",
-	1 => "What is the name of the environmental variable that stores the current user's home directory?"
+	0 => "What command can you use to set the value of an environment variable?",
+	1 => "What is the name of the environment variable that stores the current user's home directory?"
 ];
 $quizAnswers = [
 	0 => [
