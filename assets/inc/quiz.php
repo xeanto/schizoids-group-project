3cf1ae6edacc5f6432d329a6452ed22fcac5fb53
Casey Malley <casey@xeanto.us>
2023-05-01 14:46:31 -0400

    increasing modularity for quizzes

--- assets/inc/quiz.php
+++ assets/inc/quiz.php
@@ -5,11 +5,23 @@
 // $quizAnswers = array of array of strings for each question, with identifier for correct answer
 // $title = string for <h2> tag
 echo "<h2>$title</h2>";
+$count = 0;
 foreach ($quizQuestions as $question) {
-    echo "<form>";
+    echo "<form id='quiz$count'>";
     echo "<p>$question</p>";
-    foreach ($quizAnswers as $answer) {
-        echo "<label for=\"$answer\" id=\"clia$answer" . "Label\">";
-        echo "<input type=\"radio\" id=\"clia$answer\" name=\"cli\" value=\"$answer\" onclick=\"\"><br>";
+    $actualAnswer = $quizAnswers[$count][count($quizAnswers) - 1]; // get answer from array
+    $currentQuestionSet = array_slice(
+        $quizAnswers[$count],
+        0,
+        count($quizAnswers[$count]) - 1,
+        true
+    ); // remove last element since last element is correct answer
+    foreach ($currentQuestionSet as $answer) {
+        echo "<label for=\"$answer\" id=\"clia$count" . "Label\">";
+        echo "<input type=\"radio\" id=\"clia$answer\" name=\"cli\" value=\"$answer\" onclick=\"\">";
+        echo "$answer</label><br>";
     }
+    echo "<input type=\"button\" value=\"Submit\" onClick='validateQuiz(quiz$count)'>";
+    echo "</form>";
+    $count++;
 }
--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,11 +1,19 @@
 // A General function for quizzes
-function answerCorrectly(actual, excpectedID){
-    expected = document.getElementById(excpectedID);
-	label = document.getElementById(actual.id + "Label");
-    if (expected == actual){
-		label.style.color = "green";
-    }
-    else{
-        label.style.color = "red";
-    }
+function answerCorrectly(actual, excpectedID) {
+  expected = document.getElementById(excpectedID);
+  label = document.getElementById(actual.id + "Label");
+  if (expected == actual) {
+    label.style.color = "green";
+  } else {
+    label.style.color = "red";
+  }
+}
+
+function validateQuiz(quizID) {
+  form = document.getElementById(quizID);
+  selected = form.querySelectorAll("input:checked").value;
+    if (selected == true) {
+        alert("You got it right!");
+        }
+  }
 }
