62cba7649f7c0652b9058a5c5c234ee236391fb9
Casey Malley <casey@xeanto.us>
2023-05-01 12:46:54 -0400

    Fix nav log path

--- assets/inc/nav.php
+++ assets/inc/nav.php
@@ -1,42 +1,40 @@
 <body>
-    
-    	<!--menu-->
-		
-		<div class="topnav" id="myTopnav">
 
-  			<a href="index.php"><img src="assets/media/icon.png" alt="Unix Survival logo"></a>
+	<!--menu-->
 
-            <a href="history.php">History</a>
+	<div class="topnav" id="myTopnav">
 
-  			<div class="Unix">
-    			<button class="Unixbtn">Unix</button>
-    			<div class="Unix-content">
-      				<a href="cli.php">Command line interface (CLI)</a>
-      				<a href="fsn.php">File system navigation</a>
-					<a href="fman.php">File manipulation</a>
-					<a href="pman.php">Process management</a>
-					<a href="txted.php">Text editors</a>
-                    <a href="envar.php">Environmental variables</a>
-					<a href="rad.php">Redirection and piping</a>
-					<a href="perms.php">Permissions</a>
-    			</div>
-  			</div>
+		<a href="index.php"><img src="assets/media/logo.png" alt="Unix Survival logo"></a>
 
-  			<div class="Nano">
-    			<button class="Nanobtn">Nano</button>
-    			<div class="Nano-content">
-					<a href="opsvfi.php">Opening and saving files</a>
-      				<a href="basnav.php">Basic navigation</a>
-                    <a href="txtman.php">Text manipulation</a>
-                    <a href="sar.php">Searching and replacing</a>
-                    <a href="synhigh.php">Syntax highlighting</a>
-                    <a href="spchk.php">Spell-checking</a>
-      				<a href="hlp.php">Help</a>
-    			</div>
-  			</div>
+		<a href="history.php">History</a>
 
-            <a href="references.php">References</a>
-
+		<div class="Unix">
+			<button class="Unixbtn">Unix</button>
+			<div class="Unix-content">
+				<a href="cli.php">Command line interface (CLI)</a>
+				<a href="fsn.php">File system navigation</a>
+				<a href="fman.php">File manipulation</a>
+				<a href="pman.php">Process management</a>
+				<a href="txted.php">Text editors</a>
+				<a href="envar.php">Environmental variables</a>
+				<a href="rad.php">Redirection and piping</a>
+				<a href="perms.php">Permissions</a>
+			</div>
 		</div>
-		
-    
+
+		<div class="Nano">
+			<button class="Nanobtn">Nano</button>
+			<div class="Nano-content">
+				<a href="opsvfi.php">Opening and saving files</a>
+				<a href="basnav.php">Basic navigation</a>
+				<a href="txtman.php">Text manipulation</a>
+				<a href="sar.php">Searching and replacing</a>
+				<a href="synhigh.php">Syntax highlighting</a>
+				<a href="spchk.php">Spell-checking</a>
+				<a href="hlp.php">Help</a>
+			</div>
+		</div>
+
+		<a href="references.php">References</a>
+
+	</div>
