21b477a598cf96ad1ddae5fe2701761ac1fbbc64
BEIQIN CHI <beiqinchi@MacBook-Pro-234.local>
2023-05-01 21:25:19 -0400

    footer added to extras php

--- extras.php
+++ extras.php
@@ -6,3 +6,8 @@ $secondCSSFileName = ""; //Insert filename here, if no second css page is needed
 include($path . "assets/inc/header.php");
 include($path . "assets/inc/nav.php");
 ?>
+
+<?php
+//assets/inc/footer.php needs to be finished
+include($path . "assets/inc/footer.php");
+?>
