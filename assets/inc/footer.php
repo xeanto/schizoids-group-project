97524c1376d586062feae83b0e6ede8b6cc53aa0
Ryan Hamilton <rch3141@g.rit.edu>
2023-05-01 22:10:04 -0400

    editted footer

--- assets/inc/footer.php
+++ assets/inc/footer.php
@@ -7,7 +7,7 @@ if (isset($id)) {
   <img src="assets/media/logo.png" alt="Logo">
   <!--Contact info-->
   <div class="copyright">
-    <p>© 2023 Team 51 | Casey Malley, Brian Lee, Maureen McLean, Ryan Hamilton, Irwin Lin</p>
+    <p>© 2023 Group 2 | Casey Malley, Brian Lee, Maureen McLean, Ryan Hamilton, Irwin Lin</p>
     <p>Created for ISTE240 with Professor Takats </p>
   </div>
 </footer>
